3116aecb5451f7435a3e85de8b15d9357139027b
jesuis-bms <bernardbms235@gmail.com>
2026-04-17 17:27:22 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const sala4 = document.getElementById("atividades-704");
+function sala_4() {}
+addEventListener("click", sala_4);
+
+const sumario = document.getElementById("sumario");
+const sumarioBtn = document.getElementById("sumario-btn");
+
+sumarioBtn.addEventListener("click", () => sumario.classList.toggle("show"));
