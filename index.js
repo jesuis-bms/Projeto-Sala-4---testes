50321f735efedd8c19a298a75f6a0a8921aefdf6
jesuis-bms <bernardbms235@gmail.com>
2026-04-17 21:08:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ const sumario = document.getElementById("sumario");
 const sumarioBtn = document.getElementById("sumario-btn");
 
 sumarioBtn.addEventListener("click", () => sumario.classList.toggle("show"));
+
+const modal4 = document.getElementById("modal-sala4");
+const modal5 = document.getElementById("modal-sala5");
+const abrirModal5 = document.getElementById("atividades-705");
+const abrirModal4 = document.getElementById("atividades-704");
+const fecharModal4 = document.getElementById("fecharModal-sala4");
+const fecharModal5 = document.getElementById("fecharModal-sala5");
+
+function setupModal(openBtn, closeBtn, modal) {
+  openBtn.addEventListener("click", () => modal.showModal());
+  closeBtn.addEventListener("click", () => modal.close());
+}
+
+setupModal(abrirModal4, fecharModal4, modal4);
+setupModal(abrirModal5, fecharModal5, modal5);
