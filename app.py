4c96158b7cb1878cfd7ecbb54848e2b0277f6a5d
jesuis-bms <bernardbms235@gmail.com>
2026-04-22 14:01:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -121,15 +121,15 @@ def block():
 def filtroSala(sala):    
     imagens_por_atividade = {}
 
-with get_db() as conn:
-    with conn.cursor() as cursor:
-        cursor.execute("SELECT atividade_id, url FROM imagens")
-        todas = cursor.fetchall()
+    with get_db() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT atividade_id, url FROM imagens")
+            todas = cursor.fetchall()
 
-        for atividade_id, url in todas:
-            if atividade_id not in imagens_por_atividade:
-                imagens_por_atividade[atividade_id] = []
-            imagens_por_atividade[atividade_id].append(url)
+            for atividade_id, url in todas:
+                if atividade_id not in imagens_por_atividade:
+                    imagens_por_atividade[atividade_id] = []
+                    imagens_por_atividade[atividade_id].append(url)
 
     atividades_704, atividades_705 = separar_atividades_por_sala(atividades)
     logado = "usuario" in session
